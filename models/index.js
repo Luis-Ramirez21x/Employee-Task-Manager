76e4c6699e14074978f95c4841488de4d855a8ee
Luis Ramirez <luis21x@outlook.com>
2021-12-01 19:23:37 -0500

    creating models

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Task = require('./Task');
+
+User.hasMany(Task, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Task.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Task };
