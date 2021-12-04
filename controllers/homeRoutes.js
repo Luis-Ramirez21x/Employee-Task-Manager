1010066bc860c4788ca305de3296bcce8229de6b
Luis Ramirez <luis21x@outlook.com>
2021-12-04 11:38:10 -0500

    user routes and task routes created for both creationg and deletion

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,14 +6,31 @@ const withAuth = require('../utils/auth');
 //hompage route
 router.get('/', async (req, res) => {
     try {
-        const taskData = await User.findAll();
-        //const task = taskData.get({plain:true});
-        res.json(taskData);
-        //res.render('login');
+        res.render('login');  
       } catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
     });
+
+//loginn with auth view will depend on your employee role 
+router.get('/dashboard', async (req,res) => {
+  try {
+    //finding user bases on session id
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: ['name', 'role'],
+    });
+
+    //serializing
+    const user = userData.get({ plain:true });
+
+    //passing user name and role using that on front end to display certain elements
+    //possibly also pass along logged_in: req.session.logged_in
+    res.render('dashboard', {user});  
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
