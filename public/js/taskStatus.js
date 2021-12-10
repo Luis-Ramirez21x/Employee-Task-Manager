8379c5bf5a88d2e4a113351d0851e0766cad4e3a
Luis Ramirez <luis.ramirez21x@outlook.com>
2021-12-09 19:13:50 -0500

    functionality to changing task status created

--- public/js/taskStatus.js
+++ public/js/taskStatus.js
@@ -8,18 +8,14 @@ const editTask = async (event) =>{
     //get values from form 
     const taskStatus = true;
     //grab event.target.getattribute
-    const taskId = document.querySelector('#title-blog').value.trim();
+    //const taskId = document.querySelector('#title-blog').value.trim();
 
 
     
-/*
+
     if (taskStatus && description) {
         // Send a POST request to the API endpoint
-        const response = await fetch('/api/task', {
-          method: 'PUT',
-          body: JSON.stringify({ taskStatus, description }),
-          headers: { 'Content-Type': 'application/json' },
-        });
+        ;
     
         if (response.ok) {
           // If successful, redirect the browser to the profile page
@@ -29,17 +25,37 @@ const editTask = async (event) =>{
           alert(response.statusText);
           console.log('please fill out both text fields');
         }
-      }*/
+      }
 };
 
 const buttons = document.querySelectorAll(".button")
 for (const button of buttons) {
-  button.addEventListener('click', function(event){
+  button.addEventListener('click', async function(event){
     event.preventDefault()
     var element = event.target;
     console.log(element);
+
     if(element.matches('i') === true){
-        console.log('matching')
+        console.log('matching');
+        const taskStatus = true;
+        const taskId = element.getAttribute('id');
+        
+        const response = await fetch('/api/task', {
+          method: 'PUT',
+          body: JSON.stringify({ taskStatus, taskId }),
+          headers: { 'Content-Type': 'application/json' },
+        })
+
+        if (response.ok) {
+          // If successful, redirect the browser to the profile page
+          
+          document.location.reload();
+        } else {
+          alert(response.statusText);
+          console.log('please fill out both text fields');
+        }
+
+
     };
   });
 }
